984c7e66ceffae7c75c38d81acaabcdfc5b211b7
UT <tewariutkarsh@outlook.com>
2022-07-02 12:05:37 +0530

    Updated with setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 """
-equivalent to requirements.txt so that instead using requirements.txt, we can use setup.py
+equivalent to requirements.txt 
+but it is used for installing local packages
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 
@@ -13,6 +14,8 @@ VERSION="0.0.1"
 AUTHOR="Utkarsh Tewari"
 DESCRIPTION="This is my first project for current FSDS batch"
 PACKAGES=["housing"]
+LICENSE="MIT License"
+PLATFORMS="WEB"
 REQUIREMENTS_FILENAME="requirements.txt"
 
 
@@ -21,6 +24,8 @@ def get_requirements_list()->List[str]:
     """
     This function is responsible for returning the list of name of all
     the dependencies required and mentioned in requirements.txt
+    USE OF -e . = when using "pip install -r requirements.txt" in order to specify the installation of any local package present
+                  and setup.py is required for installing -e. or local packages
 
     Returns:
     --------
@@ -31,7 +36,7 @@ def get_requirements_list()->List[str]:
     
     
     with open(REQUIREMENTS_FILENAME) as requirements_file:
-        return requirements_file.readlines()
+        return requirements_file.readlines().remove("-e .")
 
 
 
@@ -42,7 +47,10 @@ setup(
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    # packages=PACKAGES,
+    packages=find_packages(),
+    license=LICENSE,
+    platforms=PLATFORMS,
     install_requires=get_requirements_list()
 
 )
