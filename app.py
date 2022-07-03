471eeea2fb66dadded175b3284954ca3f6611fb2
UT <tewariutkarsh@outlook.com>
2022-07-03 11:22:16 +0530

    Configured Exception Handling and Logger

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+import sys
 from flask import Flask, render_template, jsonify
 from flask_cors import CORS, cross_origin
+import sys
+import housing
+from housing.logger import logging
+from housing.exception_handler import HousingException
 
 
 app = Flask(__name__)
@@ -9,6 +14,15 @@ CORS(app)
 @app.route("/", methods=['POST', 'GET'])
 @cross_origin()
 def index():
+
+    try: 
+        raise Exception("We are testing custom excecption")
+
+    except Exception as e:
+        housing = HousingException(e, sys)
+        logging.info(housing.error_message)
+
+    logging.info("We are testing logging module")
     return "CI/CD Pipeline for our Project has been established"
 
 
