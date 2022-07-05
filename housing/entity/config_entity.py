65f00e97cc5b62d013659ae21f198a3c763d422c
UT <tewariutkarsh@outlook.com>
2022-07-05 11:10:34 +0530

    Updated config_entity module

---
+++ housing/entity/config_entity.py
@@ -0,0 +1,17 @@
+from collections import namedtuple
+
+
+DataIngestionConfig = namedtuple("DataIngestionConfig", 
+["dataset_download_url", "tgz_download_dir", "raw_data_dir", "ingested_train_dir", "ingested_test_dir"])
+
+DataValidationConfig = namedtuple("DataValidationConfig",
+["schema_file_path"])
+
+DataTransformationConfig = namedtuple("DataTransformationConfig",
+["add_bedroom_per_room", "transformed_train_dir", "transformed_test_dir", "preprocessed_object_file_path"])
+
+ModelTrainerConfig = namedtuple("ModelTrainerConfig",
+["train_model_file_path", "base_accuracy"])
+
+ModelPusherConfig = namedtuple("ModelPusherConfig",
+["export_dir_path"])
